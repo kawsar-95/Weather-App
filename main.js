00f4a116057e0e1b34bf4f4849f019143589c24e
kawsar-95 <virtualkawsar@gmail.com>
2020-08-14 20:31:48 +0600

    done

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
-const searchbox = document.querySelector(".search-box");
-searchbox.addEventListener("keypress", setQuery);
+const searchBox = document.querySelector(".search-box");
+searchBox.addEventListener("keypress", setQuery);
 
 function setQuery(evt) {
   if (evt.keyCode == 13) {
-    getResults(searchbox.value);
+    getResults(searchBox.value);
   }
 }
 
@@ -34,8 +34,8 @@ function displayResults(weather) {
   let weather_el = document.querySelector(".current .weather");
   weather_el.innerText = weather.weather[0].main;
 
-  let hilow = document.querySelector(".hi-low");
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
+  let hiLow = document.querySelector(".hi-low");
+  hiLow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
     weather.main.temp_max
   )}°c`;
 }
